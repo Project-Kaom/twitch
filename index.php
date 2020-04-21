36f43b9707882fa78f48d464cd2e57e5a6a850dc
Project Kaom <63582848+Project-Kaom@users.noreply.github.com>
2020-04-21 18:09:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+
+<!DOCTYPE HTML>
+<title>Hello World!</title>
+<h1>Achievements</p>
+<script src="https://extension-files.twitch.tv/helper/v1/twitch-ext.min.js"></script>
+
+<!-- index.html -->
+
 <?php
 
 print file_get_contents("achievements.md");
